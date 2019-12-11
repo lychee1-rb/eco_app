dd2936ab1e059ad60e491ce92aa31ae314e8aeb9
MayuLychee1 <akstyyahkmm.0020@gmail.com>
2019-12-12 00:28:27 +0900

    finish counter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,18 @@ class WelcomeController < ApplicationController
   end
 
   def show
+    @result = User.last
+  end
+
+  def count
+    @users = User.all
+    @user = User.new(counter: params[:key])
+    if @user.save
+      render "index"
+    end
+  end
+
+  def second
   end
 
 end
